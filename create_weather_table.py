62bb86fd20ffccd588c15c99cefa921027db6b0c
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2021-03-31 18:14:14 -0400

    added create_db code

--- create_weather_table.py
+++ create_weather_table.py
@@ -3,24 +3,42 @@ import os
 from dotenv import load_dotenv
 
 
+def generate_create_table_string():
+    create_table_string = """
+        CREATE TABLE IF NOT EXISTS weather_data (
+            id INTEGER PRIMARY KEY,
+            dt INTEGER NOT NULL,
+            description VARCHAR(255),
+            temp DECIMAL NOT NULL,
+            temp_min DECIMAL NOT NULL,
+            temp_max DECIMAL NOT NULL,
+            pressure DECIMAL NOT NULL,
+            humidity DECIMAL NOT NULL,
+            wind_spd DECIMAL NOT NULL 
+        );
+    """
+    return create_table_string
+
 def create_weather_table():
     conn = None
-    print('attempting to connect to database...')
+    print('Attempting to connect to database...')
     try:
+        # First get database credentials
         load_dotenv()
         dbname = os.getenv('dbname')
         user = os.getenv('user')
         password = os.getenv('password')
+        # Then connect to database
         conn = psycopg2.connect(f'dbname={dbname} user={user} password={password}')
-        cur = conn.cursor()
-        cur.execute('SELECT version()')
-        db_version = cur.fetchone()
-        print(db_version)
+        cur = conn.cursor()  # Get a cursor to execute commands
+        cur.execute(generate_create_table_string())  # Now execute create table command
+        cur.close()  # Close cursor connection to DB
+        conn.commit()  # Commit changes to DB
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
     finally:
         if conn is not None:
-            print('closing db connection...')
+            print('Closing connection to DB...')
             conn.close()
 
 
